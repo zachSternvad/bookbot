c1d253c8c30c395fb34857034f896ecc312734aa
zachSternvad <zachery.meck@gmail.com>
2025-11-23 19:55:53 +0100

    Refractored the code and split it into two files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_count
+
 def get_book_text(filepath):
     with open(filepath, "r") as f:
         return f.read()
@@ -5,6 +7,9 @@ def get_book_text(filepath):
 def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
-    print(text)
+ #   print(text)
+    
+    total_words = word_count(text)
+    print(f"Found {total_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,2 @@
+def word_count(text):
+    return len(text.split())
