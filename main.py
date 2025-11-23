c183931dce3a7b1b973747d84b42163bbc8cf54e
zachSternvad <zachery.meck@gmail.com>
2025-11-23 19:30:41 +0100

    Made the bookbot output a hardcoded file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath, "r") as f:
+        return f.read()
+
+def main():
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
+    print(text)
+
+main()
